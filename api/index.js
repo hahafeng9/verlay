e5cc29c14bf6162969cc39376243ac8d113c1f8c
vevc <dahaimaomi@gmail.com>
2026-07-27 22:09:47 +0800

    docs: correct project URL in welcome message

--- api/index.js
+++ api/index.js
@@ -73,7 +73,7 @@ const server = createServer((req, res) => {
       <h3>Welcome</h3>
       <p>You can visit <span style="font-weight: bold">/your-uuid</span> to view your subscription information, enjoy it ~</p>
       <h3>GitHub (Give it a &#11088; if you like it!)</h3>
-      <a href="https://github.com/vevc/nodejs-vless" target="_blank" style="color: blue">https://github.com/vevc/nodejs-vless</a>
+      <a href="https://github.com/vevc/verlay" target="_blank" style="color: blue">https://github.com/vevc/verlay</a>
     `;
     res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
     res.end(welcomeInfo);
